a0f0f7ac33d48e15a3315aa53ef9381b6a9ce96e
Brian Pennington <srsrec@gmail.com>
2018-12-23 09:17:32 -0600

    Added Upload Button, Click Event Handler

--- uploader/src/components/InputFile.js
+++ uploader/src/components/InputFile.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
 class InputFile extends React.Component {
+    state = {
+        selectedFile: null
+    }
+    
+    fileSelectedHandler = event => {
+        this.setState({
+            selectedFile: event.target.files[0]
+        })
+    }
+
+    FileUploadHandler = () => {
+
+        axios.post('gs://uploaderapp-25018.appspot.com/')
+    }
+    
     render() {
         return (
         <div className="input-file ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>File to upload</label>
-                    <input type="text" />
+                    <input type="file" onChange={this.fileSelectedHandler} />
+                    <button onClick={this.FileUploadHandler}>Upload</button>
                 </div>
             </form>
         </div>
